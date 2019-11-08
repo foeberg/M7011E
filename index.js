6fcdb12f4766a9a524f3694c7e64ae545ee6b17e
Aundron <filip@oberg.cc>
2019-11-08 16:31:59 +0100

    built first simulator version

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var Simulator = require('./simulator/');
+
+const sim = new Simulator(4);
+sim.start();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+ 
+app.get('/simulator/wind', function (req, res) {
+     res.send(sim.getWind().toString());
+});
+ 
+app.get('/simulator/householdConsumption/:houseId', function (req, res) {
+    var households = sim.getHouseholds();
+    res.send(households[req.params.houseId].getConsumption().toString());
+});
+ 
+app.get('/simulator/electricityPrice/', function (req, res) {
+    res.send(sim.getElectricityPrice().toString());
+});
+
+app.get('/simulator/', function (req, res) {
+    var wind = sim.getWind();
+    var households = sim.getHouseholds();
+    var totalConsumption = 0;
+    for(var i = 0; i < households.length; i++) {
+        totalConsumption += households[i].getConsumption();
+    }
+    var electricityPrice = sim.getElectricityPrice();
+
+    string = "Wind: " + wind + "\nTotal consumption: " + totalConsumption + "\nPrice: " + electricityPrice;
+    res.send(string);
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   
+   console.log("Example app listening at http://%s:%s", host, port);
+});
---
+++ simulator/index.js
@@ -0,0 +1,88 @@
+var gaussian = require('gaussian');
+
+class Simulator {
+    constructor(numHouseholds) {
+        this.hourOfDay = 0;
+        this.windDayDistribution = gaussian(0, 8*8);
+
+        // Average swedish house annual consumption is 25 000 kWh, divided by amount of hours in a year
+        // we get 2.85 kWh average consumtion per hour
+        // http://www.energiradgivaren.se/2011/09/elforbrukning-i-en-genomsnittlig-villa-respektive-lagenhet/
+        this.consumptionDistribution = gaussian(2.85, 0.5*0.5);
+
+        this.households = [];
+        for(var i = 0; i < numHouseholds; i++) {
+            this.households[i] = new Household(this.consumptionDistribution);
+        }
+    }
+
+    newDay() {
+        console.log("newDay");
+        this.hourOfDay = 0;
+        var daySample = this.windDayDistribution.ppf(Math.random());
+        this.windHourDistribution = gaussian(daySample, 1);
+    }
+
+    newHour() {
+        if(this.hourOfDay == 24) {
+            this.newDay();
+        }
+        console.log("newHour");
+        this.currentWind = Math.abs(this.windHourDistribution.ppf(Math.random()));
+
+        for(var i = 0; i < this.households.length; i++) {
+            this.households[i].newHour();
+        }
+
+        this.hourOfDay++;
+    }
+
+    getWind() {
+        console.log("getWind");
+        return this.currentWind;
+    }
+
+    getHouseholds() {
+        return this.households;
+    }
+
+    getElectricityPrice() {
+        var totalConsumption = 0;
+        for(var i = 0; i < this.households.length; i++) {
+            totalConsumption += this.households[i].getConsumption();
+        }
+
+        var price = totalConsumption - this.currentWind;
+        if(price < 0) {
+            price = 0;
+        }
+
+        return price;
+
+    }
+
+    start() {
+        this.newDay();
+        this.newHour();
+        setInterval(function() {
+            this.newHour();
+        }.bind(this), 10000);
+    }
+}
+
+class Household {
+    constructor(distribution) {
+        this.distribution = distribution;
+    }
+
+    getConsumption() {
+        return this.currentConsumption;
+    }
+
+    newHour() {
+        this.currentConsumption = this.distribution.ppf(Math.random());
+    }
+
+}
+
+module.exports = Simulator;
