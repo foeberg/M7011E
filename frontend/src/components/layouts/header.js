c2729ae4ba2be3bb7b5a497e77e4607604a73eee
Josefin Andersson <emmajosefinandersson@gmail.com>
2019-11-20 16:27:23 +0100

    GUI for login register and prosumer

---
+++ frontend/src/components/layouts/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './layouts.css';
+
+function Header() {
+  return (
+    <header id="headerStyle">
+      <h1 id="titleStyle">Green Lean Electrics</h1>
+    </header>
+  )
+}
+
+export default Header;
