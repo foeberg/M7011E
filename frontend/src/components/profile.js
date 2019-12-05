787449dc73478e9874101dbbe49b065f015084ad
Josefin Andersson <emmajosefinandersson@gmail.com>
2019-12-05 16:38:40 +0100

    GUI for manager profile

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import './pages/pages.css';
 import Image from './image';
+import UpdateAccount from './updateAccount';
 
 export class Profile extends Component {
+    state = {
+        updateAccount: false,
+    }
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+        document.getElementById("errorMessage").innerHTML = "";
+        }
+    changeState = () => {
+        this.setState({updateAccount: false})
+    }      
     
   render() {
     return (
@@ -11,14 +22,13 @@ export class Profile extends Component {
             <div className="flexboxRow">
                 <div style={{width: "60%", padding: "1.0em", margin: "auto"}} >   
                     <Image source={this.props.imageName} alt={"Manager"}/>
-                </div> 
-                <div className="profileContent">
+                </div>
+                <UpdateAccount updateAccount={this.state.updateAccount} changeState={this.changeState} email={this.props.email} username={this.props.username} name={this.props.name} onChange={this.onChange}/> 
+                <div className="profileContent" hidden={this.state.updateAccount}>
                     <p>Name: {this.props.name}</p>
-                    <p>E-mail:</p>
-                    <p>E-mail:</p>
-                    <p>E-mail:</p>
-                    <button type="submit" className="updateUserButton" onClick={() => {console.log("update")}}>Update account</button><br/>
-                    <button type="submit" className="deleteButton" onClick={() => {console.log("delete")}}>Delete account</button>
+                    <p>Username: {this.props.username}</p>
+                    <p>E-mail: {this.props.email}</p>
+                    <button type="submit" className="updateUserButton" onClick={() => {this.setState({updateAccount: true})}}>Update account</button>
                 </div>       
             </div>    
         </div>
