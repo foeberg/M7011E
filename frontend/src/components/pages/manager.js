55e9955a7ba533647a6fe35943af6af5ab385f8d
Josefin Andersson <emmajosefinandersson@gmail.com>
2019-12-05 16:39:27 +0100

    GUI for manager page

--- frontend/src/components/pages/manager.js
+++ frontend/src/components/pages/manager.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import './pages.css';
-import Profile from '../profile'
+import Profile from '../profile';
+import CoalPowerPlant from '../coalPowerPlant';
 
 export class Manager extends Component{
     state = {
         imageName: "placeholderManager.jpg",
-        name: "Andersson"
+        name: "Andersson",
+        username: "jossan",
+        email: "jossan@gmail.com",
+        status: "Running",
+        production: 938,
+        productionRate: 30,
+        bufferRate: 30,
+        demand: 2322
     }
 
   render() {
     return (
       <React.Fragment>
           <div className="flexboxRow">
-            <Profile imageName={this.state.imageName} name={this.state.name}/>
-            <div className="coalPowerPlantBox">
-                <h1>Coal Power plant</h1>
-            </div>
+            <Profile imageName={this.state.imageName} email={this.state.email} username={this.state.username} name={this.state.name}/>
+            <CoalPowerPlant demand={this.state.demand} bufferRate={this.state.bufferRate} status={this.state.status} production={this.state.production} productionRate={this.state.productionRate}/>
           </div>    
       </React.Fragment>
     )
