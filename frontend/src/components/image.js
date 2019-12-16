e8c85a82150b755288c62f7436d43bd2446552a5
Josefin Andersson <emmajosefinandersson@gmail.com>
2019-12-16 17:19:05 +0100

    image fix

--- frontend/src/components/image.js
+++ frontend/src/components/image.js
@@ -8,7 +8,8 @@ export class Image extends Component {
 
   state={
     selectedFile: null,
-    disableInputButton: true
+    label: "Choose image",
+    disableInputButton: true,
   }
 
   /*when a file is selected for upload, set states,update text on button with filename*/
@@ -16,6 +17,7 @@ export class Image extends Component {
     if(event.target.files[0]){
       this.setState({
           selectedFile: event.target.files[0],
+          label: event.target.files[0].name,
           disableInputButton: false
       });
       document.getElementById("choose").innerHTML = event.target.files[0].name;
@@ -32,20 +34,23 @@ export class Image extends Component {
             currentComponent.props.updateImageName(response.data);
             currentComponent.setState({
                 selectedFile: null,
+                label: "Choose image",
                 disableInputButton: true
             });
-            document.getElementById("choose").innerHTML = "Choose image";
+            $("#errorMess").css("color", "green");
+            $("#errorMess").show();
+            document.getElementById("errorMess").innerHTML= "Image uploaded";
+            setTimeout(function() { $("#errorMess").hide(); }, 2000);
         })
         .catch(function (error) {
-            console.log(error)
             $("#errorMess").show();
             document.getElementById("errorMess").innerHTML= "Could not upload image";
             setTimeout(function() { $("#errorMess").hide(); }, 2000);
             currentComponent.setState({
                 selectedFile: null,
+                label: "Choose image",
                 disableInputButton: true
             });
-            document.getElementById("choose").innerHTML = "Choose image";
         });
     }
 
@@ -55,7 +60,7 @@ export class Image extends Component {
         <div className="flexboxColumnNoFlip">
           <img className="image" src={"http://localhost:8081/householdImages/" + this.props.source} alt={this.props.alt} />
           <input type="file" name="file" id="file" className="inputfile" onInput={this.fileSelectedHandler}/>
-          <label htmlFor="file" id="choose" >Choose image</label>
+          <label htmlFor="file" id="choose" >{this.state.label}</label>
           <input className="sendButton" type="button" value="Upload" disabled = {this.state.disableInputButton} onClick={this.fileUploadHandler}/>
           <div id="errorMess" className="errorMsg" hidden={true}></div>
         </div>  
